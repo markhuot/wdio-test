db0685ccc77a0e6a32ed4c82052f34a39392b98f
Mark Huot <mark@markhuot.com>
2022-10-18 13:56:24 -0400

    removing service to match example

--- wdio.conf.js
+++ wdio.conf.js
@@ -59,10 +59,9 @@ exports.config = {
         // maxInstances can get overwritten per capability. So if you have an in-house Selenium
         // grid with only 5 firefox instances available you can make sure that not more than
         // 5 instances get started at a time.
-        maxInstances: 5,
+        // maxInstances: 5,
         //
         browserName: 'chrome',
-        acceptInsecureCerts: true,
         'wdio:devtoolsOptions': {
             headless: true
         }
@@ -119,7 +118,7 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['chromedriver'],
+    // services: ['chromedriver'],
     
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
