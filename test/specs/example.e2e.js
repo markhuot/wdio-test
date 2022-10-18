4b869e7950bbfc81faee5a67a43ccb0722e35785
Mark Huot <mark@markhuot.com>
2022-10-18 13:28:09 -0400

    local server

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,14 +1,6 @@
-const LoginPage = require('../pageobjects/login.page');
-const SecurePage = require('../pageobjects/secure.page');
-
 describe('My Login application', () => {
-    it('should login with valid credentials', async () => {
-        await LoginPage.open();
-
-        await LoginPage.login('tomsmith', 'SuperSecretPassword!');
-        await expect(SecurePage.flashAlert).toBeExisting();
-        await expect(SecurePage.flashAlert).toHaveTextContaining(
-            'You logged into a secure area!');
+    it('should load a local server', async () => {
+        await browser.url(`http://127.0.0.1:9000/`)
     });
 });
 
